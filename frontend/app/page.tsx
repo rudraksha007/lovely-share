bb2bd977cd620c9d8b91e5fe6dcea7d33046b078
AryanBaghel-1 <albertmit98@gmail.com>
2025-11-09 23:49:40 +0530

    frontend update

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+// import Header from "@/components/Header";
+import Upload from "@/components/Upload";
+
+
+export default function Page() {
+  return (
+    <div style={{ maxWidth: "600px", margin: "50px auto", textAlign: "center", color:"black",}}>
+      <h1>File Share App</h1>
+      <Upload />
+    </div>
+  );
+}
